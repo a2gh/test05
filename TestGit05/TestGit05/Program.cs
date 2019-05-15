a98a2561e121f395d3dd74c783549431d0dc28f0
AliAzami <Ali.Azami.gh@gmail.com>
2019-05-15 22:27:13 +0430

    hvjh

--- TestGit05/TestGit05/Program.cs
+++ TestGit05/TestGit05/Program.cs
@@ -12,25 +12,6 @@ namespace TestGit05
         {
             Console.WriteLine("TestGit05 Phase 01");
             Console.WriteLine("TestGit05 Phase 02");
-            
-            Console.WriteLine("TestGit05 Phase 03");
-            Console.WriteLine("TestGit05 Phase 04");
-            Console.WriteLine("TestGit05 Phase 05");
-            Console.WriteLine("TestGit05 Phase 06");
-
-            //kk
-            //jj
-
-
-
-            
-
-
-
-
-
-            //hi
-            //bye
 
             Console.ReadKey();
         }
