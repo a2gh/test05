ca67b503919e4620280c8d019c24880748cb34dc
a2gh <50555377+a2gh@users.noreply.github.com>
2019-05-15 21:17:29 +0430

    phase 00

---
+++ TestGit05/TestGit05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TestGit05 Phase 01");
+
+            Console.ReadKey();
+        }
+    }
+}
